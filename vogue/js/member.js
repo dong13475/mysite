b9f246500b819750c8e1436f70ca7d69ddad4a9d
dong13475 <travel1347@gmail.com>
2023-04-13 13:20:15 +0900

    학습

--- vogue/js/member.js
+++ vogue/js/member.js
@@ -1,7 +1,7 @@
 // 보그 PJ : 회원가입 페이지 JS - member.js
 
 ///////////// 제이쿼리 코드블록 //////////////
-$(()=>{
+$(() => {
   // 로딩확인
   console.log("회원가입 로딩완료!");
 
@@ -23,143 +23,136 @@ $(()=>{
   *********************************************/
   $(`input[type=text][id!=email2][class!=search],
   input[type=password]`)
-  // .on("blur",function(){
-  .blur(function(){
+    // .on("blur",function(){
+    .blur(function () {
+      // 0. 공백제거처리함수
+      // get rid of space -> 공백을 제거하라!
+      const groSpace = (cv) => cv.replace(/\s/g, "");
+      // 원형 : (cv) => {return cv.replace(/\s/g,"")}
+      // 정규식 : 슬래쉬(/) 사이에 표현, \s 스페이스문자
+      // 정규식 참고 -> https://www.w3schools.com/jsref/jsref_obj_regexp.asp
+      // 해석 : 공백문자를 모두(g:global-전역) 찾아서 없애시오!
+      // (빈공백으로 변경!)
 
-    // 0. 공백제거처리함수
-    // get rid of space -> 공백을 제거하라!
-    const groSpace = cv => cv.replace(/\s/g,"");
-    // 원형 : (cv) => {return cv.replace(/\s/g,"")}
-    // 정규식 : 슬래쉬(/) 사이에 표현, \s 스페이스문자
-    // 정규식 참고 -> https://www.w3schools.com/jsref/jsref_obj_regexp.asp
-    // 해석 : 공백문자를 모두(g:global-전역) 찾아서 없애시오!
-    // (빈공백으로 변경!)
-    
-    // 1. 방금 블러가 발생한 요소의 id는?
-    let cid = $(this).attr("id");
-    // cid는 current id 즉, 현재 아이디라는 뜻!
+      // 1. 방금 블러가 발생한 요소의 id는?
+      let cid = $(this).attr("id");
+      // cid는 current id 즉, 현재 아이디라는 뜻!
 
-    // 2. 블러가 발생한 요소의 입력값은? 
-    let cv = cid==="mnm" ? ($(this).val().trim()) : groSpace($(this).val());
-    // 삼항연산자 cid가 mnm이냐? 응 : 아니)
-    // let cv = $(this).val().trim();
-    // cv는 current value 즉, 현재값이란 뜻
-    // val() 메서드 - input요소의 값(value)를 읽기/쓰기용
-    // trim() 메서드 - 앞뒤공백제거(공백만 있으면 공백제거)
-    // groSpace()는 전체공백제거 함수(내가 만든것@)
-  
+      // 2. 블러가 발생한 요소의 입력값은?
+      let cv = cid === "mnm" ? $(this).val().trim() : groSpace($(this).val());
+      // 삼항연산자 cid가 mnm이냐? 응 : 아니)
+      // let cv = $(this).val().trim();
+      // cv는 current value 즉, 현재값이란 뜻
+      // val() 메서드 - input요소의 값(value)를 읽기/쓰기용
+      // trim() 메서드 - 앞뒤공백제거(공백만 있으면 공백제거)
+      // groSpace()는 전체공백제거 함수(내가 만든것@)
 
-    // 서비스 차원으로 공백제거된 데이터를 다시 입력창에 넣어줌!
-    $(this).val(cv); // val(값)
-    
-    console.log("블러",cid,cv);
+      // 서비스 차원으로 공백제거된 데이터를 다시 입력창에 넣어줌!
+      $(this).val(cv); // val(값)
 
-    /************************* 
+      console.log("블러", cid, cv);
+
+      /************************* 
       3. 빈값 여부 검사하기 
     *************************/
-    if(cv === ""){
-      // 메시지 출력
-      $(this).siblings(".msg")
-      .text("필수입력")
-      .removeClass("on"); // 빨간색글자
+      if (cv === "") {
+        // 메시지 출력
+        $(this).siblings(".msg").text("필수입력").removeClass("on"); // 빨간색글자
 
-      // 불통과!
-      pass = false;
-    }
+        // 불통과!
+        pass = false;
+      } else if (cid === "mid") {
 
-    /**************************************************** 
+      /**************************************************** 
       4. 아이디일 경우 유효성 검사 
       - 검사기준 : 영문자로 시작하는 6~20글자 영문자/숫자
     ****************************************************/
-    else if(cid === "mid"){
-      console.log("아이디검사결과:",vReg(cv,cid));
-      if(!vReg(cv,cid)){ // false일때 !(NOT연산자)로 true변경!
-        // 불통과일때 메시지
-        $(this).siblings(".msg")
-        .text("영문자로 시작하는 6~20글자 영문자/숫자")
-        .removeClass("on"); // 빨간색글자
-        // 불통과!
-        pass = false;
-      } //// if /////
-      else{ // 통과시
-        // 1. DB에 아이디가 있는지 조회후 결과로 처리해야함!(보류)
-        // 만약 아이디가 이미 있으면 "이미 사용중이거나 탈퇴한 아이디입니다"
-        // 만약 아이디가 없으면 "멋진 아이디네요!"
-  
-        // 2. 메시지 띄우기
-        $(this).siblings(".msg")
-        .text("멋진 아이디네요!")
-        .addClass("on"); // 녹색글자
-      } ///// else /////
-    } ////////// else if : 아이디 검사시 /////////
+        console.log("아이디검사결과:", vReg(cv, cid));
+        if (!vReg(cv, cid)) {
+          // false일때 !(NOT연산자)로 true변경!
+          // 불통과일때 메시지
+          $(this).siblings(".msg").text("영문자로 시작하는 6~20글자 영문자/숫자").removeClass("on"); // 빨간색글자
+          // 불통과!
+          pass = false;
+        } //// if /////
+        else {
+          // 통과시
+          // 1. DB에 아이디가 있는지 조회후 결과로 처리해야함!(보류)
+          // 만약 아이디가 이미 있으면 "이미 사용중이거나 탈퇴한 아이디입니다"
+          // 만약 아이디가 없으면 "멋진 아이디네요!"
 
-    /**************************************************** 
+          // 2. 메시지 띄우기
+          $(this).siblings(".msg").text("멋진 아이디네요!").addClass("on"); // 녹색글자
+        } ///// else /////
+      } ////////// else if : 아이디 검사시 /////////
+
+      /**************************************************** 
       5. 비밀번호일 경우 유효성 검사 
       - 검사기준 : 특수문자,문자,숫자포함 형태의 5~15자리
     ****************************************************/
-    else if(cid === "mpw"){
-      console.log("비밀번호검사결과:",vReg(cv,cid));
-      if(!vReg(cv,cid)){ // false일때 !(NOT연산자)로 true변경!
-        // 불통과일때 메시지
-        $(this).siblings(".msg")
-        .text("특수문자,문자,숫자포함 형태의 5~15자리");
+      else if (cid === "mpw") {
+        console.log("비밀번호검사결과:", vReg(cv, cid));
+        if (!vReg(cv, cid)) {
+          // false일때 !(NOT연산자)로 true변경!
+          // 불통과일때 메시지
+          $(this).siblings(".msg").text("특수문자,문자,숫자포함 형태의 5~15자리");
 
-        // 불통과!
-        pass = false;
-      } //// if /////
-      else{ // 통과시
-        // 메시지 지우기
-        $(this).siblings(".msg").empty();
-      } ///// else /////
-    } ////////// else if : 비밀번호 검사시 /////////
+          // 불통과!
+          pass = false;
+        } //// if /////
+        else {
+          // 통과시
+          // 메시지 지우기
+          $(this).siblings(".msg").empty();
+        } ///// else /////
+      } ////////// else if : 비밀번호 검사시 /////////
 
-    /**************************************************** 
+      /**************************************************** 
       6. 비밀번호확인일 경우 유효성 검사 
       - 검사기준 : 특수문자,문자,숫자포함 형태의 5~15자리
     ****************************************************/
-    else if(cid === "mpw2"){
-      if(cv !== $("#mpw").val()){ // 비밀번호가 같지않으면
-        // 불통과일때 메시지
-        $(this).siblings(".msg")
-        .text("비밀번호가 일치하지 않습니다");
+      else if (cid === "mpw2") {
+        if (cv !== $("#mpw").val()) {
+          // 비밀번호가 같지않으면
+          // 불통과일때 메시지
+          $(this).siblings(".msg").text("비밀번호가 일치하지 않습니다");
 
-        // 불통과!
-        pass = false;
-      } //// if /////
-      else{ // 통과시
-        // 메시지 지우기
-        $(this).siblings(".msg").empty();
-      } ///// else /////
-    } ////////// else if : 비밀번호확인 검사시 /////////
+          // 불통과!
+          pass = false;
+        } //// if /////
+        else {
+          // 통과시
+          // 메시지 지우기
+          $(this).siblings(".msg").empty();
+        } ///// else /////
+      } ////////// else if : 비밀번호확인 검사시 /////////
 
-    /**************************************************** 
+      /**************************************************** 
       7. 이메일 유효성 검사하기 
       - 검사기준 : 이메일 형식에 맞는지 여부검사
     ****************************************************/
-    else if(cid === "email1"){
-      // 1. 이메일 주소 만들기 : 앞주소@뒷주소
-      let comp = eml1.val() + "@" + 
-      (seleml.val() === "free" ? eml2.val() : seleml.val());
-      // (비?집:놀이동산)
+      else if (cid === "email1") {
+        // 1. 이메일 주소 만들기 : 앞주소@뒷주소
+        let comp = eml1.val() + "@" + (seleml.val() === "free" ? eml2.val() : seleml.val());
+        // (비?집:놀이동산)
 
-      // 2. 이메일 검사함수 호출하기!
-      resEml(comp);
-    } ////////// else if : 이메일 유효성 검사하기 /////////
+        // 2. 이메일 검사함수 호출하기!
+        resEml(comp);
+      } ////////// else if : 이메일 유효성 검사하기 /////////
 
-    //////// 모두 통과일 경우 메시지 지우기 //////////
-    else{
-      // 메시지 지우기 : empty() 내용지움 메서드
-      $(this).siblings(".msg").empty();
-    } ////////// else : 통과시 //////////
-    
-  }); ////////// blur ///////////
+      //////// 모두 통과일 경우 메시지 지우기 //////////
+      else {
+        // 메시지 지우기 : empty() 내용지움 메서드
+        $(this).siblings(".msg").empty();
+      } ////////// else : 통과시 //////////
+    }); ////////// blur ///////////
 
   //////////// 이메일 관련 대상선정 //////////////
-  // 이메일 앞주소 
+  // 이메일 앞주소
   const eml1 = $("#email1");
-  // 이메일 뒷주소 
+  // 이메일 뒷주소
   const eml2 = $("#email2");
-  // 이메일 선택박스 
+  // 이메일 선택박스
   const seleml = $("#seleml");
   //////////////////////////////////////////////
 
@@ -171,25 +164,22 @@ $(()=>{
     이벤트 : change - 제이쿼리 change() 메서드
     이벤트대상 : #seleml -> seleml변수
   *******************************************/
-  seleml.change(function(){
-
+  seleml.change(function () {
     // 1. 선택박스 변경된 값 읽어오기
     let cv = $(this).val();
-    console.log("선택값:",cv);
+    console.log("선택값:", cv);
 
     // 2. 선택옵션별 분기문
-    if(cv === "init"){
+    if (cv === "init") {
       // "선택해주세요"
 
       // 1. 메시지출력
-      eml1.siblings(".msg")
-      .text("이메일 옵션 선택필수!")
-      .removeClass("on"); // 빨간색글자
+      eml1.siblings(".msg").text("이메일 옵션 선택필수!").removeClass("on"); // 빨간색글자
 
       // 2. 직접 입력창 숨기기
       eml2.fadeOut(300);
     } //////////// if : 선택해주세요 /////////////
-    else if(cv === "free"){
+    else if (cv === "free") {
       // "직접입력"
 
       // 1. 직접입력창 보이기
@@ -199,9 +189,8 @@ $(()=>{
 
       // 2. 기존 메시지 지우기
       eml1.siblings(".msg").empty();
-      
     } //////////// else if : 직접입력 ////////
-    else{
+    else {
       // 이메일 주소일 경우
       // 1. 직접 입력창 숨기기
       eml2.fadeOut(300);
@@ -212,9 +201,7 @@ $(()=>{
 
       // 3. 이메일 유효성 검사함수 호출
       resEml(comp);
-
     } ////////// else : 이메일 주소 //////////
-    
   }); //////////// change //////////////
 
   /************************************************* 
@@ -232,61 +219,52 @@ $(()=>{
     -> 모든 이벤트를 함수와 연결하는 jQuery메서드는?
     on(이벤트명,함수)
   *************************************************/
-  $("#email1,#email2").on("keyup",function(){
-
+  $("#email1,#email2").on("keyup", function () {
     // 1. 현재 이벤트 대상 아이디 읽어오기
     let cid = $(this).attr("id");
 
     // 2. 현재 입력된 값 읽어오기
     let cv = $(this).val();
-    console.log("입력아이디:",cid,"\n입력값",cv);
+    console.log("입력아이디:", cid, "\n입력값", cv);
 
     // 3. 이메일 뒷주소 셋팅하기
-    let backeml = 
-    cid === "email1" ? seleml.val() : eml2.val(); 
+    let backeml = cid === "email1" ? seleml.val() : eml2.val();
     // 현재 아이디가 "email1"인가? 맞으면 선택박스
-    // 아니면 두번째 이메일뒷주소를 입력하는 중이므로 
+    // 아니면 두번째 이메일뒷주소를 입력하는 중이므로
     // 그것을 선택
     // 변수 = 조건연산자
     // 변수 = 조건식?값1:값2
 
     // 4. 만약 선택박스값이 "free"(직접입력)이면
     // 이메일 뒷주소로 변경함!
-    if(seleml.val() === "free") backeml = eml2.val();
+    if (seleml.val() === "free") backeml = eml2.val();
 
     // 5. 이메일 전체주소 조합하기
     let comp = eml1.val() + "@" + backeml;
-    
+
     // 6. 이메일 유효성 검사함수 호출
     resEml(comp);
-    
-  }); ////////// keyup ////////////// 
-  
+  }); ////////// keyup //////////////
+
   /************************************ 
     함수명 : resEml (result Email)
     기능 : 이메일 검사결과 처리
   ************************************/
   const resEml = (comp) => {
     // comp는 완성된 이메일 주소
-    console.log("이메일주소:",comp);
-    console.log("검사결과:",vReg(comp,"eml"));
+    console.log("이메일주소:", comp);
+    console.log("검사결과:", vReg(comp, "eml"));
 
     // 이메일 정규식검사에 따른 메시지 보이기
-    if(vReg(comp,"eml")){
-      eml1.siblings(".msg")
-      .text("적합한 이메일 형식입니다!")
-      .addClass("on");
+    if (vReg(comp, "eml")) {
+      eml1.siblings(".msg").text("적합한 이메일 형식입니다!").addClass("on");
     } ///////// if : 통과시 ///////////
-    else{
-      eml1.siblings(".msg")
-      .text("맞지않는 이메일 형식입니다!")
-      .removeClass("on");
+    else {
+      eml1.siblings(".msg").text("맞지않는 이메일 형식입니다!").removeClass("on");
 
       // 불통과!
       pass = false;
     } ///////// else : 불통과시 ///////////
-
-    
   }; /////////// resEml함수 ////////////
 
   /******************************************* 
@@ -307,17 +285,17 @@ $(()=>{
   let pass = true;
 
   // 이벤트 대상: #btnj
-  // 원래 서브밋버튼은 클릭시 싸고 있는 form태그의 
+  // 원래 서브밋버튼은 클릭시 싸고 있는 form태그의
   // action 설정 페이지로 모든 입력창의 값을 전송하도록
   // 설계되어 있다!
   // 기본 서브밋 이동을 막고 우리가 검사후 전송한다!
-  $("#btnj").click(e=>{
+  $("#btnj").click((e) => {
     // 호출확인
     console.log("가입해");
 
     // 1. 기본이동막기
     e.preventDefault();
-    
+
     // 2. pass 통과여부 변수에 true를 할당!
     // 처음에 true로 시작하여 검사 중간에 한번이라도
     // false로 할당되면 결과는 false가된다!
@@ -326,32 +304,79 @@ $(()=>{
     // 3. 입력창 blur이벤트 강제발생하기
     // 대상 : 블러 이벤트 발생했던 요소들!
     $(`input[type=text][id!=email2][class!=search],
-    input[type=password]`)
-    .trigger("blur");
+    input[type=password]`).trigger("blur");
 
     // 최종통과 여부
-    console.log("통과여부:",pass);
+    console.log("통과여부:", pass);
 
     // 4. 검사결과에 따라 메시지 보이기
-    if(pass){
+    if (pass) {
+      // 통과시 ////
+
+      /* 
+        [ Ajax를 이용한 POST방식으로 DB에 데이터 입력하기! ]
+
+        AJAX = Asynchronous Javascript and XML
+
+        - 비동기통신이란? 쉽게 말해서 페이지가 새로고쳐지지 않고
+        그대로 있으면서 일부분만 서버통신을 하는 것을 말한다!
+        - 제이쿼리는 POST방식으로 ajax를 할 수 있다!
+
+        [ POST방식 Ajax 메서드 ]
+        $.post(URL,data,callback)
+        $.post(전송할페이지,전송할데이터,전송후실행함수)
+      */
+
+      $.post(
+        // 1. 전송할페이지 : 서브밋할 페이지
+        "./process/ins.php",
+        // 2. 전송할데이터 : 객체형식 {속성:값}
+        {
+          // 1. 아이디
+          "mid":$("#mid").val(),
+          // 2. 비밀번호
+          "mpw":$("#mpw").val(),
+          // 3. 이름
+          "mnm":$("#mnm").val(),
+          // 4. 성별
+          "gen":$(":radio[name=gen]:checked").val(),
+          // 5. 이메일 앞주소
+          "email1":$("#email1").val(),
+          // 6. 이메일 뒷주소(선택박스)
+          "seleml":$("#seleml").val(),
+          // 7. 이메일 뒷주소(직접입력)
+          "email2":$("#email2").val(),
+        },
+        // 3. 전송후실행함수 : 
+        // -> 익명함수로 실행후 결과리턴받음!
+        // -> 이 부분이 Promise 처리된 것임!
+        function(res){ // res - 리턴된 결과받는 변수 
+          console.log(res);
+        }
+
+
+      ); /////////// post 메서드 /////////////
+
+      // 일단 페이지테스트를 워해 기본 서브밋해준다!
+      // $(".logF").submit();
+      // submit() - 폼요소를 서브밋해주는 메서드
+
       // 원래는 post방식으로 DB에 회원가입정보를
       // 전송하여 입력후 DB처리완료시 성공메시지나
       // 로그인 페이지로 넘겨준다!
       alert("회원가입을 축하드립니다! 짝짝짝!!!");
+
       // 로그인페이지로 리디렉션!
       // location.href = "login.html";
 
       // 브라우저 캐싱 히스토리를 지우려면
       // location.replace(url)을 사용한다!
-      location.replace("login.html");
-
+      // location.replace("login.html");
     } ////////// if : 통과시 //////////
-    else{
+    else {
       alert("입력을 수정하세요~!");
     } ////////// else : 불통과시 ////////////
-    
   }); ////////// click /////////////
-  
 }); ///////// JQB ///////////////////////
 
 /*////////////////////////////////////////////////////////
@@ -361,7 +386,7 @@ $(()=>{
     -> w3c참조
     https://www.w3schools.com/jsref/jsref_obj_regexp.asp
 
-/*////////////////////////////////////////////////////////
+/*/ ///////////////////////////////////////////////////////
 function vReg(val, cid) {
   // val - 검사할값, cid - 처리구분아이디
   // //console.log("검사:"+val+"/"+cid);
@@ -388,8 +413,7 @@ function vReg(val, cid) {
       // (?=.*[!@#$%^&+=]) 특수문자 사용체크!
       break;
     case "eml": // 이메일
-      reg =
-        /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;
+      reg = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;
       // 이메일 형식에 맞는지 검사하는 정규식
       break;
   } //////////// switch case문 //////////////////
